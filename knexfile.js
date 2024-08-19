7811d7c89ec5c2682763bc518ac064a44b908969
Kassym Turysbek <kasym012@gmail.com>
2024-08-20 01:32:07 +0500

    seedsAdded

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,12 @@
 module.exports = {
     development: {
         client: 'pg',
-         connection: process.env.DATABASE_URL,
+        connection: {
+            database: "skilltest",
+            host: "localhost",
+            user: "postgres",
+            password: "12345"
+        },
         migrations: {
             directory: __dirname + '/db/migrations',
         },
@@ -15,7 +20,7 @@ module.exports = {
     },
     production: {
         client: 'pg',
-          connection: process.env.DATABASE_URL,
+        connection: process.env.DATABASE_URL,
         migrations: {
             directory: __dirname + '/db/migrations',
         },
